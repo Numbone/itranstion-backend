dba531f05bb27a0c7e2eb33176628c98fe4141c7
Numbone <nurlybekqwerty@gmail.com>
2025-08-01 06:13:52 +0500

    update migrations

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { env } from "@/common/utils/envConfig";
 import { app, logger } from "@/server";
 import { pool } from "./common/utils/db";
+import fs from "fs";
+import path from "path";
+
+const runMigrations = async () => {
+	try {
+		const sql = fs.readFileSync(path.resolve(__dirname, "init.sql"), "utf8");
+		await pool.query(sql);
+		logger.info("Таблицы созданы / обновлены");
+	} catch (error) {
+		logger.error("Ошибка при выполнении миграции", error);
+		process.exit(1);
+	}
+};
 
 const server = app.listen(env.PORT, async() => {
 	await pool.connect();
+	await runMigrations();
 	const { NODE_ENV, HOST, PORT } = env;
 	logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
 });
