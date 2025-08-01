45364cdc5bb2588fb491735e39f4f81725c160ac
Numbone <nurlybekqwerty@gmail.com>
2025-08-01 06:20:13 +0500

    delete  ratelimiter

--- src/common/middleware/rateLimiter.ts
+++ src/common/middleware/rateLimiter.ts
@@ -1,5 +1,5 @@
 import type { Request } from "express";
-import { rateLimit } from "express-rate-limit";
+import { rateLimit } from 'express-rate-limit';
 
 import { env } from "@/common/utils/envConfig";
 
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
+import { openAPIRouter } from "@/api-docs/openAPIRouter";
+import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
+import { userRouter } from "@/api/user/userRouter";
+import errorHandler from "@/common/middleware/errorHandler";
+import requestLogger from "@/common/middleware/requestLogger";
+import { env } from "@/common/utils/envConfig";
 import cors from "cors";
 import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
-import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
-import { userRouter } from "@/api/user/userRouter";
-import { openAPIRouter } from "@/api-docs/openAPIRouter";
-import errorHandler from "@/common/middleware/errorHandler";
-import rateLimiter from "@/common/middleware/rateLimiter";
-import requestLogger from "@/common/middleware/requestLogger";
-import { env } from "@/common/utils/envConfig";
 import { authRouter } from "./api/auth/authRouter";
 import { authenticate } from "./common/middleware/auth";
 
@@ -23,7 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(helmet());
-app.use(rateLimiter);
 
 // Request logging
 app.use(requestLogger);
@@ -40,3 +38,4 @@ app.use(openAPIRouter);
 app.use(errorHandler());
 
 export { app, logger };
+
