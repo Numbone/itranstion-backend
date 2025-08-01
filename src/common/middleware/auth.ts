dd01e13ea924e3d618c8afdbf4f3851e24354884
Numbone <nurlybekqwerty@gmail.com>
2025-08-01 10:23:31 +0500

    update auth middlewate

--- src/common/middleware/auth.ts
+++ src/common/middleware/auth.ts
@@ -1,25 +1,39 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { env } from "../utils/envConfig";
-
+import { pool } from "../utils/db";
 
 interface JwtPayload {
   userId: number;
 }
 
-export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: " Unauthorized: No token provided" });
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, env.JWT_SECRET) as JwtPayload;
-    (req as any).user = decoded; 
+    const userId = decoded.userId;
+
+    const result = await pool.query("SELECT id, status FROM users WHERE id = $1", [userId]);
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized: User not found" });
+    }
+
+    if (user.status === "blocked") {
+      return res.status(401).json({ message: "Unauthorized: User account is blocked" });
+    }
+
+    (req as any).user = { id: user.id, status: user.status };
     next();
+
   } catch (err) {
     return res.status(401).json({ message: "Unauthorized: Invalid token" });
   }
